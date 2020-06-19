f6dcbe899e49b900fbe0f2f31beeca4ca5828642
Lingyan Wang <40675419+lingyanw@users.noreply.github.com>
2020-06-19 09:34:51 -0600

    fix: added if_exists = 'replace' in df.to_sql to avoid the error with existing dataset

--- data/process_data.py
+++ data/process_data.py
@@ -47,7 +47,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('data', engine, index=False)  
+    df.to_sql('data', engine, index=False, if_exists = 'replace')  
 
 
 def main():
